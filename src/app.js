5b0fe8e1a8f3cfa2266a57c3c63f6dced1da17ce
jpablop123 <juanpablopinto123@icloud.com>
2023-04-13 22:17:56 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let Andy = document.getElementById("myexcuse");
+
+window.onload = function juanp() {
+  let quien = ["daniel", "odont", "milo", "roland"];
+  let que = ["se comio", "se orino", "dano", "rompio"];
+  let quecosa = ["mi tarea", "las llaves", "el carro"];
+  let cuando = [
+    "antes de clase",
+    "a tiempo",
+    "cuando termine",
+    "en almuerzo",
+    "mientras rezaba",
+  ];
+  let variableloca = Math.random() * 3;
+
+  let quienIndx = Math.floor(variableloca);
+  let queIndx = Math.floor(variableloca);
+  let quecosaIndx = Math.floor(variableloca);
+  let cuandoIndx = Math.floor(variableloca);
+
+  return (
+    quien[quienIndx] +
+    " " +
+    que[queIndx] +
+    " " +
+    quecosa[quecosaIndx] +
+    " " +
+    cuando[cuandoIndx]
+  );
 };
+Andy.innerHTML = juanp();
